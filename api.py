104ea85772e43b2402b17e42a3e9ae94c72ae9e1
Natalie Fang <nfang61@gmail.com>
2024-09-03 09:41:50 +0100

    backend

--- api.py
+++ api.py
@@ -1,15 +1,10 @@
 from fastapi import FastAPI
-
-app = FastAPI()
-
-
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import StreamingResponse
 from PIL import Image
 import io
 
 app = FastAPI()
-
 @app.get("/")
 def root():
     return "Hello World"
